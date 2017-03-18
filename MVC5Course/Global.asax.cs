0371b39698b6a7c041f9c12acb8eff19089e733f
wechen@uuu <te426odin@gmail.com>
2017-03-18 13:37:54 +0800

    建立ActionResult 於global.asax中新增 可降低viewEngines 搜尋耗能

--- MVC5Course/Global.asax.cs
+++ MVC5Course/Global.asax.cs
@@ -18,6 +18,10 @@ namespace MVC5Course
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            //viewEngines在搜尋VIEW的時候可以跳過WebForm的view
+            ViewEngines.Engines.Clear();
+            ViewEngines.Engines.Add(new RazorViewEngine());
         }
     }
 }
